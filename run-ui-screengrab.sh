ecd6b15334a467ecec8e2b1167e545855e1dd0d0
somkiat <somkiat.p@gmail.com>
2016-03-29 04:11:40 +0700

    Script of screengrab

--- run-ui-screengrab.sh
+++ run-ui-screengrab.sh
@@ -1,1 +1,6 @@
+export ANDROID_HOME=/Users/somkiat/Library/Android/sdk
+export PATH=.:$ANDROID_HOME/platform-tools/:$PATH
 ./gradlew assembleDebug assembleAndroidTest
+fastlane ui
+rm fastlane/metadata/android/th/images/phoneScreenshots/*
+cp fastlane/screengrab/*/images/*/*.png fastlane/metadata/android/th/images/phoneScreenshots/
