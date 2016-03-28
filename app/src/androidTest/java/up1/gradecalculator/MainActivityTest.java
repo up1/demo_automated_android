9c8ad399684cf6fb114a64d836009a0cf929ab9c
somkiat <somkiat.p@gmail.com>
2016-03-29 02:11:42 +0700

    Demo for screengrab

--- app/src/androidTest/java/up1/gradecalculator/MainActivityTest.java
+++ app/src/androidTest/java/up1/gradecalculator/MainActivityTest.java
@@ -11,12 +11,19 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
 
     @Rule
     public ActivityTestRule<MainActivity> mMainActivityTestRule
@@ -24,14 +31,17 @@ public class MainActivityTest {
 
     @Test
     public void convert_score_80_should_display_grade_A() {
+        Screengrab.screenshot("step_01");
         //Arrange
-        onView(withId(R.id.score))
-                .perform(typeText("80"));
+        onView(withId(R.id.score)).perform(typeText("80"));
+        Screengrab.screenshot("step_02");
 
         //Act
         onView(withId(R.id.convertButton)).perform(click());
+        Screengrab.screenshot("step_03");
 
         //Assert
         onView(withId(R.id.result)).check(matches(withText("You got grade A")));
+        Screengrab.screenshot("step_04");
     }
 }
