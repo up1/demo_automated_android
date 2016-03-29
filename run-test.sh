5547519ed0780d72c05f0c3df7cfe57141ccd608
somkiat <somkiat.p@gmail.com>
2016-03-29 12:05:02 +0700

    Script to build and run unit testing

--- run-test.sh
+++ run-test.sh
@@ -1,2 +1,4 @@
 export ANDROID_HOME=/Users/somkiat/Library/Android/sdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_40.jdk/Contents/Home
+export PATH=.:$JAVA_HOME/bin:$PATH
 ./gradlew clean test
